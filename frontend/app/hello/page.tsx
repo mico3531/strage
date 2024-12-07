ff6391090a144672612a97352e1106f4e32856a8
mico3531 <oshirukosentai@gmail.com>
2024-12-08 00:12:12 +0900

    Initial commit

---
+++ frontend/app/hello/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return <h1>Hello, Next.js!</h1>
+}
+
+// function の前に export default と置くことでページロード時に自動で関数が読み込まれる
+
+// return は関数の戻り値
+// JSXによりHTMLタグを直接戻り値として使える
+
+// 拡張子 tsx はJSXを用いたTypeScriotを指す
